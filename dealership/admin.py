eb1a4db9b4fd6136dd1c4290ccbf2c3ff9237107
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-02-21 19:57:57 +0100

    Customizing admin page

--- dealership/admin.py
+++ dealership/admin.py
@@ -3,8 +3,29 @@ from django.contrib import admin
 
 from . import models
 
+@admin.register(models.Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['type','manufacturer','model','horsepower','cubic_meters','year','price']
+    list_editable = ['price']
+    lists_per_page = 10
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','email']
+    list_editable = ['email']
+    ordering = ['first_name','last_name']
+    lists_per_page = 10
+
+@admin.register(models.UserServiceVehicle)
+class UserServiceVehicle(admin.ModelAdmin):
+    list_display = ['user','vehicle','placed_at','payment_status','service_status']
+    list_editable = ['payment_status']
+    lists_per_page = 10
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['company_name','CEO']
+    list_editable = ['CEO']
+
 admin.site.register(models.Type)
-admin.site.register(models.Company)
-admin.site.register(models.Vehicle)
-admin.site.register(models.User)
-admin.site.register(models.UserServiceVehicle)
+
+
